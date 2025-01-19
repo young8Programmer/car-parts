1520ff0c80e27fd2b9c359a1a842035ad1421164
Vali <valireyimbergenov79@gmail.com>
2025-01-19 19:50:20 +0500

    postgresql url create

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
-import { PartsModule } from './parts/parts.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Part } from './parts/entities/part.entity';
 import { AuthModule } from './auth/auth.module';
 import { User } from './auth/entities/auth.entity';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { PartsModule } from './parts/parts.module';
+import * as fs from 'fs';
 
 @Module({
   imports: [
@@ -15,11 +16,16 @@ import { join } from 'path';
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      url: 'postgresql://vali:wIN4RPOJDLTQ9Qo14AJlvQ@tender-monarch-7282.j77.aws-eu-central-1.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full',
-      entities: [Part, User],
-      synchronize: true,
+      host: 'tender-monarch-7282.j77.aws-eu-central-1.cockroachlabs.cloud',  // Host
+      port: 26257,  // Port
+      database: 'defaultdb',  // Database nomi
+      username: 'vali',  // Foydalanuvchi nomi
+      password: 'wIN4RPOJDLTQ9Qo14AJlvQ',  // Parol
+      entities: [Part, User],  // Entity-lar
+      synchronize: true,  // Avtomatik jadvallarni yaratish
       ssl: {
-      rejectUnauthorized: false,
+        rejectUnauthorized: false,  // SSL tekshiruvi
+        ca: fs.readFileSync('C:/Users/valir/AppData/Roaming/postgresql/root.crt').toString(),  // Sertifikat
       },
     }),
     PartsModule,
