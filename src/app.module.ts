ff7c7e67edf2b4e0c45ed9ef7ea6309dec787ba8
Vali <valireyimbergenov79@gmail.com>
2025-01-19 17:26:10 +0500

    postgresql url create

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,7 +15,7 @@ import { join } from 'path';
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      url: 'postgresql://postgres:nUqFvdrnZfEGNrDdrzzrvvDWxWJkHHZt@junction.proxy.rlwy.net:33398/railway',
+      url: 'postgresql://postgres:nUqFvdrnZfEGNrDdrzzrvvDWxWJkHHZt@postgres.railway.internal:5432/railway',
       entities: [Part, User],
       synchronize: true,
       ssl: {
