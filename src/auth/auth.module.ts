6912d2d46792db2f3864068b4236ddc5c5ef3ad0
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:10:27 +0500

    style: kod uslubini yaxshilash

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 // unit testlar qo'shildi
+// environment variables sozlandi
 import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/auth.entity'; 
