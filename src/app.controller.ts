caa1e1e1989eb654447b7d111429a478eb602f67
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:10:06 +0500

    test: unit testlar qo'shildi

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+// changelog yangilandi
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
