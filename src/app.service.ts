d499d5b7e2f62dc03751a9f9facf35dc3f38db2e
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:10:12 +0500

    docs: installation qo'llanmasi yaratildi

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 // componentlarni qayta tashkilash
 export class AppService {
+// CI/CD pipeline sozlandi
   getHello(): string {
 // database querylarni optimallashtirish
 // user authentication qo'shildi
