6604b3fa1db564cef4957e8d2822c30c97a412ac
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:10:27 +0500

    feat: admin dashboard yaratildi

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
+// unit testlar qo'shildi
 import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/auth.entity'; 
