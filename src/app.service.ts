57ed799d46e0ffaff1bfd7d4aaaec1ee9a678b18
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:10:11 +0500

    test: database testlari qo'shildi

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
+// database querylarni optimallashtirish
     return 'Hello World!';
   }
 }
